7f30047a1597cf9092130c409701868ba7c4e6fd
sir-kinuthia-001 <denniskinuthia1105@gmail.com>
2021-01-16 22:45:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-// this js file will get data from arduino store it in a firebase database.
+/* this js file will get data from arduino store it in a firebase database.
 var dweetclient = require("node-dweetio");
 var dweetio = new dweetclient();
 //this file is also a data analysis engine
 var teperature_data_per_minute = temperature_data/60;
-console.log(temperature_data_per_minute)
+console.log(temperature_data_per_minute)*/
+//javascript basic server
+const http = require("http");
+const fs = require ("fs").promises;
+//address $ port
+const hostname = "localhost";
+const port ="8081";
+
+const server = http.createServer((req,res)=> {
+  //file to host -> index.html
+  fs.readFile(__dirname + "/index.html").then((content)=>{
+    res.statusCode = 200;
+    res.setHeader("content-type","text.html");
+    res.end(content);
+  });
+});
+server.listen(port, hostname,()=>{
+  console.log(`server is running at http://${hostname}:${port}`)
+});
